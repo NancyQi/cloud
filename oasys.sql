f0f8fbef2f17cd380b97a37febfa7f279ba4115f
NancyQi <48859763+NancyQi@users.noreply.github.com>
2021-05-22 12:34:15 +0800

    Update oasys.sql

--- oasys.sql
+++ oasys.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.29, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: oasys
 -- ------------------------------------------------------
--- Server version	5.7.29
+-- Server version	8.0.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
